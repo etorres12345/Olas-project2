8f999023469eb8b431913c6862da26f96946e6f9
etorres12345 <erik.torres1215@gmail.com>
2023-09-12 17:10:12 +0200

    search by keyword add

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,20 +2,32 @@ const router = require("express").Router();
 const Post = require("../models/Post.model");
 
 // GET route to display form to create post
-router.get('/post-create', (req, res, next) => {
-    res.render('posts/create.hbs')
+router.get("/post-create", (req, res, next) => {
+  res.render("posts/create.hbs");
 });
 
 // GET route to display all the posts
 router.get("/posts", (req, res, next) => {
-    Post.find()
-        // .populate('author')
-        .then(allPosts => {
-            console.log("All posts in DB:", allPosts);
-            res.render('posts/posts.hbs', { posts: allPosts })
-        })
-        .catch(error => next(error));
+  Post.find()
+    // .populate('author')
+    .then((allPosts) => {
+      console.log("All posts in DB:", allPosts);
+      res.render("posts/posts.hbs", { posts: allPosts });
+    })
+    .catch((error) => next(error));
 });
 
+router.post("/posts/search", (req, res) => {
+  const keyword = req.body.keyword;
+
+  Post.find({ $text: { $search: keyword } })
+    .then((posts) => {
+      res.render("posts/posts.hbs", { posts });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    });
+});
 
 module.exports = router;
