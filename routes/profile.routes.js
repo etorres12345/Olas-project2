c4a4e376712ba0100c42690fba421c669a1cf8ae
Da-vinia <daviniatosco@gmail.com>
2023-09-12 18:24:13 +0200

    create profile get route

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,17 +1,40 @@
 const router = require("express").Router();
 const { isLoggedOut } = require('../middleware/route-guard');
+const User = require("../models/User.model");
+// const fileUploader = require('../../config/cloudinary.config');
 
 router.get("/profile", isLoggedOut, (req, res) => {
   res.render("profile-views/my-profile");
 });
 
-router.get("/profile/create", (req, res) => {
-  res.render("profile-views/create-profile");
-});
+// router.get("/profile/:profileId", isLoggedOut, (req, res) => {
+//   const {userId} = req.params;
 
-router.get("/profile/edit", (req, res) => {
-  res.render("profile-views/edit-profile");
-});
+//   User.findById(userId) 
+//     .then(theUser => res.render("profile-views/my-profile", {user: theUser}))
+//     .catch(error => {
+//       next(error);
+//     });
+// });
+
+// router.post("/profile",  isLoggedOut, (req, res, next) => {
+//   // const {_id} = currentUser;
+//   const { username, avatar } = req.body;
+
+//   User.create({ username, avatar } )
+  
+// });
+
+
+// router.get("/profile/edit", isLoggedOut, (req, res) => {
+//   res.render("profile-views/edit-profile");
+// });
+
+// router.post("/profile/edit", isLoggedOut, fileUploader.single("user-image"), (req, res, next) => {
+//   const newImg = req.file.path;   
+//   const {_id} = currentUser;
+
+// })
 
 router.get("/profile/delete", (req, res) => {
   res.render("profile-views/delete-profile");
