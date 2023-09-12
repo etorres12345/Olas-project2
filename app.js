513d56ce688402eef31d5be9879527802a67259a
Da-vinia <daviniatosco@gmail.com>
2023-09-12 11:43:16 +0200

    create login&logout

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+require("./config/session.config")(app);
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
@@ -30,6 +31,9 @@ app.use("/", indexRoutes);
 
 const profileRoute = require("./routes/profile.routes");
 app.use("/", profileRoute);
+
+const loginRoutes = require("./routes/auth/login.routes");
+app.use("/", loginRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
