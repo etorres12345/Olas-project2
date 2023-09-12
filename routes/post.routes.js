a7939001d2e005d3c9ec4d0d431ff41f7b8f9b76
etorres12345 <erik.torres1215@gmail.com>
2023-09-12 14:20:19 +0200

    add form/routes/hbs file for search filter

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,20 +2,44 @@ const router = require("express").Router();
 const Post = require("../models/Post.model");
 
 // GET route to display form to create post
-router.get('/post-create', (req, res, next) => {
-    res.render('posts/create.hbs')
+router.get("/post-create", (req, res, next) => {
+  res.render("posts/create.hbs");
 });
 
 // GET route to display all the posts
 router.get("/posts", (req, res, next) => {
-    Post.find()
-        // .populate('author')
-        .then(allPosts => {
-            console.log("All posts in DB:", allPosts);
-            res.render('posts/posts.hbs', { posts: allPosts })
-        })
-        .catch(error => next(error));
+  Post.find()
+    // .populate('author')
+    .then((allPosts) => {
+      console.log("All posts in DB:", allPosts);
+      res.render("posts/posts.hbs", { posts: allPosts });
+    })
+    .catch((error) => next(error));
 });
 
+router.get("/posts/filter/:category", (req, res) => {
+  const selectedCategory = req.params.category;
+
+  Post.find({ category: selectedCategory })
+    .then((filteredPosts) => {
+      res.render("posts/filtered-posts", {
+        posts: filteredPosts,
+        selectedCategory: selectedCategory,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error fetching filtered posts.");
+    });
+});
+
+router.post("/posts/filter", (req, res) => {
+  const selectedCategory = req.body.category;
+  const filteredPosts = [];
+  res.render("posts/filtered-posts", {
+    posts: filteredPosts,
+    selectedCategory,
+  });
+});
 
 module.exports = router;
