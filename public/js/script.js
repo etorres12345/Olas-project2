0c99718437812453e51e4a035efd1a906a469171
Da-vinia <daviniatosco@gmail.com>
2023-09-14 18:14:09 +0200

    style profile page

--- public/js/script.js
+++ public/js/script.js
@@ -3,24 +3,25 @@
 //   console.log("olas-project2 JS imported successfully!");
 // });
 
-// const profilePencilIcon = document.getElementById("profilePencilIcon");
-// const popupEditForm = document.getElementById("popupEditForm");
-// const closePopupBtn = document.getElementById("closePopupBtn");
-// const deleteProfileBtn = document.getElementById("deleteProfileBtn");
+//profile page
+const profilePencilIcon = document.getElementById("profilePencilIcon");
+const popupEditForm = document.getElementById("popupEditForm");
+const closePopupBtn = document.getElementById("closePopupBtn");
+const deleteProfileBtn = document.getElementById("deleteProfileBtn");
 
-// profilePencilIcon.addEventListener("click", () => {
-//   popupEditForm.style.display = "block";
-// });
-// closePopupBtn.addEventListener("click", () => {
-//   popupEditForm.style.display = "none";
-// });
-// deleteProfileBtn.addEventListener("click", () => {
-//   const isConfirmed = confirm("Are you suere you want to leave the surfers community?");
+profilePencilIcon.addEventListener("click", () => {
+  popupEditForm.style.display = "block";
+});
+closePopupBtn.addEventListener("click", () => {
+  popupEditForm.style.display = "none";
+});
+deleteProfileBtn.addEventListener("click", () => {
+  const isConfirmed = confirm("Are you suere you want to leave the surfers community?");
 
-//   if(isConfirmed) {
-//     window.location.href = "/";
-//   }
-// });
+  if(isConfirmed) {
+    window.location.href = "/";
+  }
+});
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
@@ -81,3 +82,4 @@ window.addEventListener("load", function() {
   
   audio.play();
 });
+
