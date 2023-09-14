831c82a0d7d78b193a44071eb2bf9e5e363302cc
Da-vinia <daviniatosco@gmail.com>
2023-09-14 20:01:56 +0200

    style create post

--- public/js/script.js
+++ public/js/script.js
@@ -4,24 +4,47 @@
 // });
 
 //profile page
-const profilePencilIcon = document.getElementById("profilePencilIcon");
-const popupEditForm = document.getElementById("popupEditForm");
-const closePopupBtn = document.getElementById("closePopupBtn");
-const deleteProfileBtn = document.getElementById("deleteProfileBtn");
+// const profilePencilIcon = document.getElementById("profilePencilIcon");
+// const popupEditForm = document.getElementById("popupEditForm");
+// const closePopupBtn = document.getElementById("closePopupBtn");
+// const deleteProfileBtn = document.getElementById("deleteProfileBtn");
 
-profilePencilIcon.addEventListener("click", () => {
-  popupEditForm.style.display = "block";
-});
-closePopupBtn.addEventListener("click", () => {
-  popupEditForm.style.display = "none";
-});
-deleteProfileBtn.addEventListener("click", () => {
-  const isConfirmed = confirm("Are you suere you want to leave the surfers community?");
+// profilePencilIcon.addEventListener("click", () => {
+//   popupEditForm.style.display = "block";
+// });
+// closePopupBtn.addEventListener("click", () => {
+//   popupEditForm.style.display = "none";
+// });
+// deleteProfileBtn.addEventListener("click", () => {
+//   const isConfirmed = confirm("Are you suere you want to leave the surfers community?");
 
-  if(isConfirmed) {
-    window.location.href = "/";
-  }
+//   if(isConfirmed) {
+//     window.location.href = "/";
+//   }
+// });
+document.addEventListener("DOMContentLoaded", () => {
+  const profilePencilIcon = document.getElementById("profilePencilIcon");
+  const popupEditForm = document.getElementById("popupEditForm");
+  const closePopupBtn = document.getElementById("closePopupBtn");
+  const deleteProfileBtn = document.getElementById("deleteProfileBtn");
+
+  profilePencilIcon.addEventListener("click", () => {
+    popupEditForm.style.display = "block";
+  });
+
+  closePopupBtn.addEventListener("click", () => {
+    popupEditForm.style.display = "none";
+  });
+
+  deleteProfileBtn.addEventListener("click", () => {
+    const isConfirmed = confirm("Are you sure you want to leave the surfers community?");
+
+    if (isConfirmed) {
+      window.location.href = "/";
+    }
+  });
 });
+
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
